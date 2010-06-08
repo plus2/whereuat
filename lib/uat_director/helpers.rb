fc3a3583b4ba62842359cbd77236a2b9f7e0b558
Ben Askins <ben.askins@gmail.com>
2010-06-08 16:48:04 +1000

    Pull in js from github

--- lib/uat_director/helpers.rb
+++ lib/uat_director/helpers.rb
@@ -1,29 +1,7 @@
 module UatDirector
   module Helpers
     def uat_director
-<<TDSCRIPT
-<script type="text/javascript">
-  $("body").prepend("#{toolbar_html}");
-  $("head").append("<link href='http://github.com/plus2/uat_director/raw/master/lib/uat_director/stylesheets/uat_director.css' media='screen' rel='stylesheet' type='text/css'>");
-  $(function() {
-    $("#uat-bar a.go").click(function() {
-      $.ajax({
-        url: '/uat_director',
-        success: function(response) {
-          $(".pop-over").append(response); 
-          $(".pop-over").slideDown();
-        }
-      });
-    });
-
-    $("#uat-bar a.close").click(function() {
-      $(".pop-over").slideUp(function() {
-        $(".pop-over ol").remove();
-      });
-    });
-  });
-</script>
-TDSCRIPT
+      "<script type='text/javascript' src='http://github.com/plus2/uat_director/raw/master/lib/uat_director/javascripts/uat_director.js'></script>"
     end
 
     def toolbar_html
