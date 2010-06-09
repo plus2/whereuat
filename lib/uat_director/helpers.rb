71ca578732509160b2f7c579ea4388802e40e874
Ben Askins <ben.askins@gmail.com>
2010-06-09 12:21:11 +1000

    Move javascript out of helper

--- lib/uat_director/helpers.rb
+++ lib/uat_director/helpers.rb
@@ -1,33 +1,7 @@
 module UatDirector
   module Helpers
     def uat_director
-<<TDSCRIPT
-<script type="text/javascript">
-  $("body").prepend("#{toolbar_html}");
-  $("head").append("<link href='http://github.com/plus2/uat_director/raw/master/lib/uat_director/stylesheets/uat_director.css' media='screen' rel='stylesheet' type='text/css'>");
-  $(function() {
-    $("#uat-bar a.go").click(function() {
-      $.ajax({
-        url: '/uat_director',
-        success: function(response) {
-          $(".pop-over").append(response); 
-          $(".pop-over").slideDown();
-        }
-      });
-    });
-
-    $("#uat-bar a.close").click(function() {
-      $(".pop-over").slideUp(function() {
-        $(".pop-over ol").remove();
-      });
-    });
-  });
-</script>
-TDSCRIPT
-    end
-
-    def toolbar_html
-      "<div id='uat-bar'><a class='go' href='#'>Show me what to test</a><div class='pop-over hide'><a class='close' href='#'>close</a></div></div>"
+      "<script type='text/javascript' src='http://github.com/plus2/uat_director/raw/master/lib/uat_director/javascript/uat_director.js'></script>"
     end
   end
 end
