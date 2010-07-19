3c584ed494392df07d36388f3cee89e25e70454b
lachie <lachiec@gmail.com>
2010-07-19 10:43:14 +1000

    correct js variable name

--- lib/uat_director/helpers.rb
+++ lib/uat_director/helpers.rb
@@ -1,7 +1,7 @@
 module UatDirector
   module Helpers
     def uat_director
-      "<script type='text/javascript'>var uatdirector_stylesheet_url=#{UatDirector.configuration.whereuat_stylesheet_url.to_json};</script>" +
+      "<script type='text/javascript'>var whereuat_stylesheet_url=#{UatDirector.configuration.whereuat_stylesheet_url.to_json};</script>" +
       "<script type='text/javascript' src='#{UatDirector.configuration.whereuat_javascript_url}'></script>"
     end
   end
