cc68b8ddb4c208e1f0fbe025eec50c42fa6bc775
lachie <lachiec@gmail.com>
2010-08-14 17:24:42 +1000

    remove all the url config we now don't need

--- lib/whereuat/configuration.rb
+++ lib/whereuat/configuration.rb
@@ -1,32 +1,6 @@
 module Whereuat
-
   class Configuration
     attr_accessor :pivotal_tracker_token
     attr_accessor :pivotal_tracker_project
-
-    attr_accessor :whereuat_stylesheet_url
-    attr_accessor :whereuat_javascript_url
-
-    def initialize
-      if Whereuat.assets_path.directory?
-        use_internal_resources!
-      else
-        use_github_resources!
-      end
-    end
-
-    def use_github_resources!
-      puts "using github resources"
-      self.whereuat_stylesheet_url = "http://github.com/plus2/whereuat/raw/master/lib/whereuat/stylesheets/whereuat.css"
-      self.whereuat_javascript_url = "http://github.com/plus2/whereuat/raw/master/lib/whereuat/javascript/whereuat.js"
-    end
-
-    def use_internal_resources!
-      puts "using internal resources"
-      self.whereuat_stylesheet_url = "/whereuat/whereuat.css"
-      self.whereuat_javascript_url = "/whereuat/whereuat.js"
-    end
-    alias :use_dev_resources! :use_internal_resources!
   end
-
 end
